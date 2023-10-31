1a3b7469e1858b96f645404d3d3c3a44234bae18
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2023-10-31 08:50:46 +0200

    Tehtavat 7.5 ja 7.6

--- routed-anecdotes/src/components/CreateNew.jsx
+++ routed-anecdotes/src/components/CreateNew.jsx
@@ -19,6 +19,12 @@ const CreateNew = (props) => {
     navigate("/anecdotes");
   };
 
+  const handleReset = () => {
+    content.onReset();
+    author.onReset();
+    info.onReset();
+  };
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -35,7 +41,10 @@ const CreateNew = (props) => {
           url for more info
           <input {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="reset" onClick={handleReset}>
+          reset
+        </button>
       </form>
     </div>
   );
